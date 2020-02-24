05d81a9219cdb3b9ebce6c18fff47fc84c16f24e
maratfatkullov <fatk-marat@yandex.ru>
2020-02-24 11:31:16 +0400

    Add condition for check empty cookie value

--- src/token-decoder.js
+++ src/token-decoder.js
@@ -6,13 +6,20 @@ module.exports = async function (context) {
         console.log(`[header] Skip setting default header ${name}. Already set to ${value}`);
         return;
     }
+
     context.request.setHeader('X-CSRF-Token', `${csrf_token}`);
     console.log(`[header] Set X-CSRF-Token header: ${csrf_token}`);
 };
 
-async function decodeCsrfToken(context) {
+async function decodeCsrfToken(context) {    
     const csrf_token = context.request.getEnvironmentVariable('xsrf_token');
-    let decoded_csrf_token = decodeURIComponent(csrf_token);
-    console.log(decoded_csrf_token);
-    return decoded_csrf_token;
+    console.log('marat');
+    console.log(csrf_token)
+    if (!csrf_token.length || csrf_token.includes("request 'cookie'")) {
+        return '';
+    } else {
+        let decoded_csrf_token = decodeURIComponent(csrf_token);
+        console.log(decoded_csrf_token);
+        return decoded_csrf_token;
+    }
 }
